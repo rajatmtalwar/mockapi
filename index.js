edebb2c42a7c09f9bf1f455932305d1cac84d15d
Rajat Talwar <rajat.talwar@sap.com>
2020-05-08 19:38:54 +0530

    Added linting and formatting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const httpProxy = require('http-proxy');
-let mockData = require('./data/Mock');
-const config = require('./config/config.js');
+const express = require("express");
+const httpProxy = require("http-proxy");
+let mockData = require("./data/Mock");
+const config = require("./config/config.js");
 const app = express();
 const port = config.httpServerPort || 80;
 const targetProxyHost = config.targetProxyHost;
@@ -10,60 +10,64 @@ const proxy = enableProxyForward ? httpProxy.createProxyServer({}) : null;
 
 const mockDataFiles = config.mockDataFiles || [];
 
-mockDataFiles.forEach(filePath => {
-    try{
-        mockData = mockData.concat(require(filePath));
-    }catch(e) {
-        console.log(e);
-    }
-    
+proxy.on("error", function (e) {
+  console.log(e);
+});
+
+mockDataFiles.forEach((filePath) => {
+  try {
+    mockData = mockData.concat(require(filePath));
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 function getConfigForRequest(req) {
-    const url = req.url;
-    const method = req.method;
-    let result = mockData.filter((config) => {
-        if(url.includes(config.path) && (config.method.toLowerCase() === method.toLowerCase())) {
-            return true;
-        }
-        return false;
-    });
-    return result && (result.length>0) && result[0];
+  const url = req.url;
+  const method = req.method;
+  const result = mockData.filter((config) => {
+    if (
+      url.includes(config.path) &&
+      config.method.toLowerCase() === method.toLowerCase()
+    ) {
+      return true;
+    }
+    return false;
+  });
+  return result && result.length > 0 && result[0];
 }
 
 function proxyRequest(req, res) {
-    const proxyHeaders = config.proxyRequestHeaders || [];
-    proxyHeaders.forEach((headerObject)=>{
-        req.headers[headerObject.header] = headerObject.value;
-    });
-    
-    proxy.web(req, res, { target: targetProxyHost });
+  const proxyHeaders = config.proxyRequestHeaders || [];
+  proxyHeaders.forEach((headerObject) => {
+    req.headers[headerObject.header] = headerObject.value;
+  });
+
+  proxy.web(req, res, { target: targetProxyHost });
 }
 
-app.all('*', function(req, res) {
-    let config = getConfigForRequest(req);
-    let response = null;
-    if(typeof config.getResponse === 'function') {
-        response = config.getResponse(req);
-        if(response) {
-            if(enableProxyForward && (response.forwardRequest === true)) {
-                proxyRequest(req, res);
-                return;
-            }
-            let responseCode = response.responseCode || 200;
-            let responseDataObject = response.responseDataObject || {};
-            res.writeHead(responseCode, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(responseDataObject), 'UTF-8');
-            return;
-        }
-        
-    }
-    if(enableProxyForward) {
+app.all("*", function (req, res) {
+  const config = getConfigForRequest(req);
+  let response = null;
+  if (typeof config.getResponse === "function") {
+    response = config.getResponse(req);
+    if (response) {
+      if (enableProxyForward && response.forwardRequest === true) {
         proxyRequest(req, res);
-    }else{
-        res.end();
+        return;
+      }
+      const responseCode = response.responseCode || 200;
+      const responseDataObject = response.responseDataObject || {};
+      res.writeHead(responseCode, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(responseDataObject), "UTF-8");
+      return;
     }
-
+  }
+  if (enableProxyForward) {
+    proxyRequest(req, res);
+  } else {
+    res.end();
+  }
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
